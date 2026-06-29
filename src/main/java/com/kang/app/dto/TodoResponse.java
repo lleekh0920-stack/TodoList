cfb2b8c99a27fba07bdfe40e846b1af5ea3b35ca
lleekh0920 <lleekh0920@gmail.com>
2026-06-29 16:30:28 +0900

    검색

--- src/main/java/com/kang/app/dto/TodoResponse.java
+++ src/main/java/com/kang/app/dto/TodoResponse.java
@@ -2,6 +2,10 @@ package com.kang.app.dto;
 
 import com.kang.app.entity.Todo;
 import lombok.Getter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 public class TodoResponse {
@@ -9,11 +13,15 @@ public class TodoResponse {
     private Long id;
     private String content;
     private boolean completed;
+    private LocalDateTime createdAt;
+    private LocalDateTime updateAt;
 
     public TodoResponse(Todo todo){
         this.id= todo.getId();
         this.content=todo.getContent();
         this.completed=todo.isCompleted();
+        this.createdAt=todo.getCreatedAt();
+        this.updateAt=todo.getUpdateAt();
     }
 
 }
